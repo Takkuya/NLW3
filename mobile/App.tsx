64e070df120ccd83d34fff49110fabc152be6873
Takkuya <takuyagabriel@gmail.com>
2020-10-17 21:48:21 -0300

    Início do projeto mobile

---
+++ mobile/App.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import MapView, {Marker, Callout, PROVIDER_GOOGLE} from 'react-native-maps'
+
+import mapMarker from './src/images/map-marker.png'
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <MapView 
+        provider={PROVIDER_GOOGLE}
+        style={styles.map} 
+        initialRegion={{
+          latitude:-23.5514897,
+          longitude: -46.5066368,
+          latitudeDelta: 0.008,
+          longitudeDelta : 0.008,
+        }}
+      >
+        <Marker 
+          icon={mapMarker}
+          coordinate={{
+            latitude:-23.5514897,
+            longitude: -46.5066368,
+          }}
+        >
+          <Callout tooltip={true}>
+            <View style={styles.calloutContainer}>
+               <Text style={styles.calloutText}>Lar das meninas</Text>
+            </View>
+          </Callout>
+        </Marker>
+      </MapView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height
+  },
+
+  calloutContainer: {
+    width: 160,
+    height: 46,
+    paddingHorizontal: 16,
+    backgroundColor: 'rgba(255, 255, 255, 0.8)',
+    borderRadius: 16,
+    justifyContent: 'center',
+  },
+
+  calloutText: {
+    color: '#0089a5',
+    fontSize: 14,
+  }
+});
