c23503c35ebe1fea6a74779254cb7c90b2610afc
Takkuya <takuyagabriel@gmail.com>
2020-10-18 21:50:40 -0300

    Conteúdo passado durante a aula do dia 16/10/2020

--- mobile/src/routes.tsx
+++ mobile/src/routes.tsx
@@ -7,11 +7,15 @@ const {Navigator, Screen} = createStackNavigator()
 
 import OrphanagesMap from './pages/OrphanagesMap'
 import OrphanagesDetails from './pages/OrphanagesDetails'
+import SelectMapPosition from './pages/CreateOrphanage/SelectMapPosition'
+import OrphanageData from './pages/CreateOrphanage/OrphanageData'
+
+import Header from './components/Header'
 
 export default function Routes() {
     return(
         <NavigationContainer>
-            <Navigator screenOptions={{headerShown: false}}>
+            <Navigator screenOptions={{headerShown: false, cardStyle: {backgroundColor: '#f2f3f5'}}}>
                 <Screen 
                     name="OrphanagesMap" 
                     component={OrphanagesMap} 
@@ -21,7 +25,28 @@ export default function Routes() {
                 <Screen 
                     name="OrphanagesDetails" 
                     component={OrphanagesDetails} 
-                    options={{}}
+                    options={{
+                        headerShown: true,
+                        header: () => <Header showCancel={false} title="Orfanato"/>
+                    }}
+                />
+
+                <Screen 
+                    name="SelectMapPosition" 
+                    component={SelectMapPosition} 
+                    options={{
+                        headerShown: true,
+                        header: () => <Header title="Selecione no mapa"/>
+                    }}
+                />
+
+                <Screen 
+                    name="OrphanageData" 
+                    component={OrphanageData} 
+                    options={{
+                        headerShown: true,
+                        header: () => <Header title="Informe os dados"/>
+                    }}
                 />
             </Navigator>
         </NavigationContainer>
